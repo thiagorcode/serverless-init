a2c7eb90c1b629f4e05f15f81c6e852a8bbec5cd
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-24 19:53:35 -0300

    feat: save data in dynamoDb

---
+++ save_data_in_dynamodb/serverless.ts
@@ -0,0 +1,84 @@
+import type { AWS } from '@serverless/typescript';
+
+
+const serverlessConfiguration: AWS = {
+  service: 'send-file',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-dynamodb-local', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { 
+    exportTransactions: {
+      handler: "src/functions/exportTransactions.handler",
+      events: [
+        {
+          http: {
+            path: 'export',
+            method: 'post',
+            cors: true
+          }
+        }
+      ]
+    },
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+    dynamodb: {
+      stages: ["dev", "local"],
+    start: {
+      port:8000,
+      inMemory: true,
+      migrate: true
+    }
+    }
+  },
+  resources: {
+    Resources: {
+      dbFileUsers: {
+        Type:"AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users_file_export",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S"
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'id',
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
+};
+
+module.exports = serverlessConfiguration;
